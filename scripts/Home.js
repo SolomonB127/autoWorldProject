3cf8cfc0d4224b508c6fce3a84a1f3383675ae19
SolomonB127 <solomonakinsulire@gmail.com>
2023-08-05 16:35:43 +0100

    Homepage responsive, added pagination to cars list and reviews to homepage

--- scripts/Home.js
+++ scripts/Home.js
@@ -6,3 +6,11 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+// Review slider
+document.addEventListener('DOMContentLoaded', function () {
+  var myCarousel = document.querySelector('#customerReviewSlider');
+  var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 3000, // Adjust the interval (optional)
+  });
+});
